0a5cf4b7fc715c65be77d1ed284c720256d3e9f9
Andrew Smith <diver.vlz@gmail.com>
2023-10-15 18:10:41 +0300

    first React component

---
+++ frontend/src/SpeakerProfile.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class SpreakerProfile extends React.Component {
+    render() {
+        return <h1>This is a class component from SpreakerProfile</h1>
+    }
+}
+
+export default SpreakerProfile
