27c969e6721888d5a9fcf86b71e39dae2014a553
Andrew Smith <diver.vlz@gmail.com>
2023-10-15 19:06:44 +0300

    some update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,29 @@
 import './App.css';
 import React from 'react';
-import SpreakerProfile from './SpeakerProfile';
+import Profile from './Profile';
 
-const spekerName = 'Artem Efimov'
+
+function SpreakerProfile(props) {
+  return(
+    <>
+      <h3>Staff: {props.name}</h3>
+      <p>Position: {props.jobTitle}</p>
+      <p>Job role: {props.role}</p>
+    </>
+  );
+}
+
 
 function App() {
   return (
     <div>
-      <h1>Welcome to Dispatcher app, {spekerName}!</h1>
-      <h3>
-        <SpreakerProfile />
-      </h3>
+      <h1>Welcome to Dispatcher app!</h1>
+        <SpreakerProfile 
+          name='Artem Efimov'
+          jobTitle='St.Dispatcher'
+          role='Main Place'
+        />
+      <Profile />
     </div>
   );
 }
--- frontend/src/SpeakerProfile.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-class SpreakerProfile extends React.Component {
-    render() {
-        return <h1>This is a class component from SpreakerProfile</h1>
-    }
-}
-
-export default SpreakerProfile
